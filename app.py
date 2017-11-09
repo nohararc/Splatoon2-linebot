c67df339c06ac429ed22302920f56a7cb6570f54
yusuke <yusuke@home>
2017-11-10 00:16:57 +0900

    は?

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage("は？"))
 
 
 if __name__ == "__main__":
