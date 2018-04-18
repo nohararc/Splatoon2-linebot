5b58e180476d35fd28e2bc5f8a69530d36b9d9e2
nohararc <rubick3433@gmail.com>
2018-04-18 22:54:54 +0900

    fix tests format

--- tests/test_ika.py
+++ tests/test_ika.py
@@ -14,43 +14,50 @@ from linebot.models import (
 import app
 
 class TestSalmon(unittest.TestCase):
-    def test_salmon(self):
-        file_dir = os.path.dirname(__file__)
-        webhook_sample_json_path = os.path.join(file_dir, 'webhook.json')
-        with open(webhook_sample_json_path, encoding="utf-8") as fp:
-            body = fp.read()
 
-        parser = WebhookParser('channel_secret')
-        # mock
-        parser.signature_validator.validate = lambda a, b: True
+    file_dir = os.path.dirname(__file__)
+    webhook_sample_json_path = os.path.join(file_dir, 'webhook.json')
+    with open(webhook_sample_json_path, encoding="utf-8") as fp:
+        body = fp.read()
 
-        events = parser.parse(body, 'channel_secret')
+    # mock
+    parser = WebhookParser('channel_secret')
+    parser.signature_validator.validate = lambda a, b: True
+    events = parser.parse(body, 'channel_secret')
+
+    def setUp(self):
         app.line_bot_api.reply_message = MagicMock()
-        app.handle_message(events[0])
 
-        res = app.line_bot_api.reply_message.call_args
-        _, textsendmessage = res[0]
+    def test_salmon(self):
+        app.handle_message(self.events[0])
+        _, textsendmessage = app.line_bot_api.reply_message.call_args[0]
         self.assertRegex(textsendmessage[0].text, r'(\d{2}/\d{2} \d{2}:\d{2}) ～ (\d{2}/\d{2} \d{2}:\d{2})\n(.+)')
 
-class TestRandomRule(unittest.TestCase):
-    def test_random_rule(self):
-        file_dir = os.path.dirname(__file__)
-        webhook_sample_json_path = os.path.join(file_dir, 'webhook.json')
-        with open(webhook_sample_json_path, encoding="utf-8") as fp:
-            body = fp.read()
+class TestRandom(unittest.TestCase):
 
-        parser = WebhookParser('channel_secret')
-        # mock
-        parser.signature_validator.validate = lambda a, b: True
+    file_dir = os.path.dirname(__file__)
+    webhook_sample_json_path = os.path.join(file_dir, 'webhook.json')
+    with open(webhook_sample_json_path, encoding="utf-8") as fp:
+        body = fp.read()
 
-        events = parser.parse(body, 'channel_secret')
+    # mock
+    parser = WebhookParser('channel_secret')
+    parser.signature_validator.validate = lambda a, b: True
+    events = parser.parse(body, 'channel_secret')
+
+    def setUp(self):
         app.line_bot_api.reply_message = MagicMock()
-        app.handle_message(events[1])
 
-        res = app.line_bot_api.reply_message.call_args
-        _, textsendmessage = res[0]
+    def test_random_rule(self):
+        app.handle_message(self.events[1])
+        _, textsendmessage = app.line_bot_api.reply_message.call_args[0]
         self.assertRegex(textsendmessage[0].text, r"(ナワバリ)|(ガチ)(ヤグラ|ホコ|エリア|アサリ)")
 
+    def test_random_weapon(self):
+        app.handle_message(self.events[2])
+        res = app.line_bot_api.reply_message.call_args
+        _, textsendmessage = res[0]
+        self.assertRegex(textsendmessage[0].text, r"ブキ: .+\nサブ: .+\nスペシャル: .+")
 
 
 if __name__ == '__main__':
