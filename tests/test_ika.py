667bd9d7f1c4c87a72053398df86ec935282275f
nohararc <rubick3433@gmail.com>
2018-04-22 02:19:45 +0900

    added test

--- tests/test_ika.py
+++ tests/test_ika.py
@@ -55,9 +55,62 @@ class TestRandom(unittest.TestCase):
 
     def test_random_weapon(self):
         app.handle_message(self.events[1])
-        res = app.line_bot_api.reply_message.call_args
-        _, textsendmessage = res[0]
+        _, textsendmessage = app.line_bot_api.reply_message.call_args[0]
         self.assertRegex(textsendmessage[0].text, r"ブキ: .+\nサブ: .+\nスペシャル: .+")
+
+class TestStage(unittest.TestCase):
+
+    file_dir = os.path.dirname(__file__)
+    stage_json_path = os.path.join(file_dir, 'stage.json')
+    with open(stage_json_path, encoding="utf-8") as fp:
+        body = fp.read()
+
+    # mock
+    parser = WebhookParser('channel_secret')
+    parser.signature_validator.validate = lambda a, b: True
+    events = parser.parse(body, 'channel_secret')
+
+    def setUp(self):
+        app.line_bot_api.reply_message = MagicMock()
+
+    def test_stage(self):
+        app.handle_message(self.events[0])
+        _, textsendmessage = app.line_bot_api.reply_message.call_args[0]
+        self.assertRegex(textsendmessage[0].text, r"(\d{2}/\d{2} \d{2}:\d{2}) ～ (\d{2}/\d{2} \d{2}:\d{2})\n(ガチ)(ヤグラ|ホコ|エリア|アサリ)\n.+\n.+")
+
+        app.handle_message(self.events[1])
+        _, textsendmessage = app.line_bot_api.reply_message.call_args[0]
+        self.assertRegex(textsendmessage[0].text, r"(\d{2}/\d{2} \d{2}:\d{2}) ～ (\d{2}/\d{2} \d{2}:\d{2})\n(ガチ)(ヤグラ|ホコ|エリア|アサリ)\n.+\n.+")
+
+        app.handle_message(self.events[2])
+        _, textsendmessage = app.line_bot_api.reply_message.call_args[0]
+        self.assertRegex(textsendmessage[0].text, r"(\d{2}/\d{2} \d{2}:\d{2}) ～ (\d{2}/\d{2} \d{2}:\d{2})\nナワバリバトル\n.+\n.+")
+
+    def test_stage_time_odd(self):
+        pass
+
+    def test_stage_time_even(self):
+        pass
+
+class TestWeapon(unittest.TestCase):
+
+    file_dir = os.path.dirname(__file__)
+    stage_json_path = os.path.join(file_dir, 'stage.json')
+    with open(stage_json_path, encoding="utf-8") as fp:
+        body = fp.read()
+
+    # mock
+    parser = WebhookParser('channel_secret')
+    parser.signature_validator.validate = lambda a, b: True
+    events = parser.parse(body, 'channel_secret')
+
+    def setUp(self):
+        app.line_bot_api.reply_message = MagicMock()
+
+    def test_weapon(self):
+        app.handle_message(self.events[0])
+        _, textsendmessage = app.line_bot_api.reply_message.call_args[0]
+        self.assertRegex(textsendmessage[0].text, r"(\d{2}/\d{2} \d{2}:\d{2}) ～ (\d{2}/\d{2} \d{2}:\d{2})\n(ガチ)(ヤグラ|ホコ|エリア|アサリ)\n.+\n.+")
 
 
 if __name__ == '__main__':
